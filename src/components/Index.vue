d293024c16a708e51733bff0b05611fd9170c54e
Yuki <yuki.mul.tiplus@gmail.com>
2017-06-21 01:59:02 +0900

    first commit

---
+++ src/components/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="pomodoro-timer">
+    <header>
+      <h1>Pomodoro Timer</h1>
+      <nav>
+        <ul>
+          <li>FAQ</li>
+        </ul>
+      </nav>
+    </header>
+    <pomodoro-timer></pomodoro-timer>
+  </div>
+</template>
+
+<script>
+import PomodoroTimer from '@/components/PomodoroTimer'
+
+export default {
+  components: {
+    'pomodoro-timer': PomodoroTimer
+  }
+}
+</script>
---
+++ src/components/PomodoroTimer.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="timer-body">
+    <p>{{ time }}</p>
+    <ul>
+      <li><button @click="start()" :disabled="enableToStart">Start</button></li>
+      <li><button @click="stop()">Stop</button></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      time: '00:00',
+      timerId: '',
+      isBreak: false,
+      enableToStart: false
+    }
+  },
+  methods: {
+    start: function() {
+      const self = this
+
+      self.enableToStart = true
+
+      let secs = self.isBreak ? 5 * 60 : 25 * 60
+      self.timerId = window.setInterval(() => {
+        secs--
+        let m = Math.floor(secs / 60)
+        let s = secs % 60
+
+        if (m < 10) {
+          m = "0" + m
+        }
+        if (s < 10) {
+          s = "0" + s
+        }
+
+        self.time = m + ":" + s
+
+        if (secs === 0) {
+          self._clearTimer()
+        }
+      }, 1000)
+    },
+    stop: function() {
+      this.enableToStart = false
+      this._clearTimer()
+    },
+    _clearTimer: function() {
+      this.time = "00:00"
+      this.isBreak = this._reverse(this.isBreak)
+      window.clearInterval(this.timerId)
+    },
+    _reverse: function(bool) {
+      return bool ? false : true
+    }
+  },
+  beforeDestroy() {
+    this._clearTimer()
+  }
+}
+</script>
